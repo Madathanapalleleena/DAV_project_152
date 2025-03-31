2feccb5a1a7115e4bea6cc492dc8a01bcb2a79c9
Madathanapalleleena <leena91919@gmail.com>
2025-03-31 14:41:09 +0530

    Added main.py file containing operations on the dataset

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np 
+import pandas as pd 
+# Loading the dataset into a Pandas DataFrame
+df = pd.read_csv('Global_Music_Streaming_Listener_Preferences.csv')
+
+print("displaying first 5 rows of the dataset")
+print(df.head())
+
